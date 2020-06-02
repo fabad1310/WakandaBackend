efec001a47c4d88385624b1b08a9714e07e42b0a
Martín Miranda <debianitram@gmail.com>
2020-06-02 19:16:10 -0300

    NO SE OLVIDEN DE AGREGAR EL PROFILE USUARIO EN CLIENTE

--- wakandabackend/clientes/models.py
+++ wakandabackend/clientes/models.py
@@ -16,5 +16,7 @@ class Clientes(models.Model):
     nombre = models.CharField(max_length=30, null=False, blank=False)
     tipo = models.CharField(max_length=1, choices=TIPO_CLIENTE, default=REVENDEDOR)
 
+    # Agregar profile de usuario.
+
     def __str__(self):
         return self.nombre
